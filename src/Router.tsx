41c0dab03affc8f1c198706b5e9538a3968ca0d0
Maria Luiza Rodrigues <marialuiza.r.eng@gmail.com>
2024-09-24 17:38:08 -0300

    feat: fixing all paths

--- src/Router.tsx
+++ src/Router.tsx
@@ -13,13 +13,14 @@ export function Router() {
   return (
     <Routes>
       <Route element={<DefaultLayout />}>
-        <Route path="/home" element={<Home />} />
-        <Route path="/movies/" element={<Movies />} />
-        <Route path="/tv-series/" element={<TVSeries />} />
-        <Route path="/bookmarked/" element={<Bookmarked />} />
+        <Route path="home" element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="tv-series" element={<TVSeries />} />
+        <Route path="bookmarked" element={<Bookmarked />} />
       </Route>
-      <Route path="/auth" element={<LoginLayout />}>
-        <Route path="/sign-up/" element={<SignUp />} />
+      <Route element={<LoginLayout />}>
+        <Route path="/" element={<Login />} />
+        <Route path="sign-up" element={<SignUp />} />
       </Route>
     </Routes>
   );
